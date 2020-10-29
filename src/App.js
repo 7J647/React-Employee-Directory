086b47214f06192a1805791652edd0ffa3813e8d
7J647 <007J647@gmail.com>
2020-10-28 22:21:11 -0400

    added search box

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 function App() {
   return (
     <>
-    <div style={{backgroundColor:"rgb(0,24,65)", height:130, borderBottom: "5px solid red"}}
+    <header style={{backgroundColor:"rgb(0,24,65)", height:130, borderBottom: "5px solid red"}}
               className="header">
-    {/* <Row>
-          <Col size="sm-12"> */}
             <h1 style={{color:"white", textAlign:"center"}}>Employee Directory</h1>
             <p style={{color:"white", textAlign:"center"}}>Click on carrots to filter by heading or use the search box to narrow your results.</p>
-          {/* </Col>   
-    </Row> */}
+    </header>
+    <div style={{margin:20, display: "flex", justifyContent: "center"}}>
+      <input style={{height:45, width: 200, borderRadius:5, 
+        borderColor:"#E8E7EB"}}type="search" id="search" placeholder="Search">
+        </input>
     </div>
+
+
+
+    
     </>
     
 );
