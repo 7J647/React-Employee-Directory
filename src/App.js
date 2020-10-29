7946012b235a309bf0b63f26f15662666a9e4c51
7J647 <007J647@gmail.com>
2020-10-28 21:42:55 -0400

    header styling complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import './App.css';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <>
+    <div style={{backgroundColor:"rgb(0,24,65)", height:130, borderBottom: "5px solid red"}}
+              className="header">
+    {/* <Row>
+          <Col size="sm-12"> */}
+            <h1 style={{color:"white", textAlign:"center"}}>Employee Directory</h1>
+            <p style={{color:"white", textAlign:"center"}}>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+          {/* </Col>   
+    </Row> */}
     </div>
+    </>
+    
 );
   }
 
