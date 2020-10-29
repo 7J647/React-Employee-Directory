6a49db250e38036eb053c33b1b4263df318353b9
7J647 <007J647@gmail.com>
2020-10-28 22:26:50 -0400

    added component SearchResultContainer with state

---
+++ src/components/SearchResultContainer.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SearchResultContainer extends Component {
+    
+    state = {
+        search: "",
+        results: []
+    };
+    
+    
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SearchResultContainer;
